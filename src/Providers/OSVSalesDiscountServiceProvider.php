6e2cdf4585eba233525fbf1dcd28eeb3cee4ee3b
Kay Kästner <admin@MacBook-Pro-2.local>
2026-03-19 12:36:10 +0100

    v1.1.0: Umbau von DataProvider/Container auf ShopBuilder-Widget

--- src/Providers/OSVSalesDiscountServiceProvider.php
+++ src/Providers/OSVSalesDiscountServiceProvider.php
@@ -3,6 +3,7 @@
 namespace OSVSalesDiscount\Providers;
 
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Modules\ContentBuilder\Contracts\ContentWidgetRepositoryContract;
 
 class OSVSalesDiscountServiceProvider extends ServiceProvider
 {
@@ -12,5 +13,9 @@ class OSVSalesDiscountServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        pluginApp(ContentWidgetRepositoryContract::class)
+            ->registerWidget(
+                'OSVSalesDiscount\\Widgets\\DiscountBannerWidget'
+            );
     }
 }
